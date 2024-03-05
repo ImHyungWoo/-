4ceb545aeb66faee0a0f9d6bba647288332b047b
ImHyungWoo <rihan4675@gmail.com>
2024-03-05 12:59:33 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,50 @@
+import os
+import win32com.client
+import requests
+ 
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+ 
+myToken = os.environ.get("SLACK_API_TOKEN")
+
+if myToken is None:
+    print("myToken.")
+    exit()
+ 
+post_message(myToken,"#stock","안녕하세요.")
+
+
+ # 채널변경 시 원하는 채널 우클릭 후 들어가 앱 추가를 통해 봇을 넣어야지 다른 채널에도 적용이 가능
+ 
+# 연결 여부 체크
+objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
+bConnect = objCpCybos.IsConnect
+if (bConnect == 0):
+    print("PLUS가 정상적으로 연결되지 않음. ")
+    exit()
+ 
+# 종목코드 리스트 구하기
+objCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
+codeList = objCpCodeMgr.GetStockListByMarket(1) #거래소
+codeList2 = objCpCodeMgr.GetStockListByMarket(2) #코스닥
+ 
+ 
+print("거래소 종목코드", len(codeList))
+for i, code in enumerate(codeList):
+    secondCode = objCpCodeMgr.GetStockSectionKind(code)
+    name = objCpCodeMgr.CodeToName(code)
+    stdPrice = objCpCodeMgr.GetStockStdPrice(code)
+    print(i, code, secondCode, stdPrice, name)
+ 
+print("코스닥 종목코드", len(codeList2))
+for i, code in enumerate(codeList2):
+    secondCode = objCpCodeMgr.GetStockSectionKind(code)
+    name = objCpCodeMgr.CodeToName(code)
+    stdPrice = objCpCodeMgr.GetStockStdPrice(code)
+    print(i, code, secondCode, stdPrice, name)
+ 
+print("거래소 + 코스닥 종목코드 ",len(codeList) + len(codeList2))
